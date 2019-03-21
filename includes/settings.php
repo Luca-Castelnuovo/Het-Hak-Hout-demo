22d0e1c11975fd27110d98ee9996daa11a2ae76f
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 17:42:14 +0100

    ipdate

--- includes/settings.php
+++ includes/settings.php
@@ -4,7 +4,7 @@ return (object) [
     'database' => (object) [
         'host' => 'localhost',
         'user' => 'overlangbroek_db',
-        'password' => 'PASSWORD',
+        'password' => '766hnWNZuNqUdyqEZDJgNl2ErZ11z2ZVNgwWzL1NTqEPngUKKD7ojW7o7hwTEMzI.',
         'database' => 'overlangbroek_db',
     ],
 ];
