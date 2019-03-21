32ea0625f31a039a4dd2fed722ab1faf2e881b9e
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 17:24:20 +0100

    update

---
+++ build/vogels.php
@@ -0,0 +1,27 @@
+<?php require $_SERVER['DOCUMENT_ROOT'].'/includes/init.php'; ?>
+<?php echo page_header('Overlangbroek', 'vogels'); ?>
+<div class="row">
+    <div class="col s12 m3">
+        <a href="/Buizerd.html"
+            ><div class="card">
+                <div class="card-image">
+                    <img src="/static/images/Buizerd.jpg" />
+                </div>
+                <div class="card-content smallbox">
+                    <span class="card-title black-text">Buizerd</span>
+                </div>
+            </div></a
+        >
+    </div>
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <img src="/static/images/Koolmees.jpg" />
+            </div>
+            <div class="card-content smallbox">
+                <span class="card-title black-text">Koolmees</span>
+            </div>
+        </div>
+    </div>
+</div>
+<?php echo page_footer(); ?> 
---
+++ buizerd.php
@@ -0,0 +1,5 @@
+<?php require $_SERVER['DOCUMENT_ROOT'].'/includes/init.php'; ?> <?php echo
+page_header('Overlangbroek', 'vogels'); ?>
+
+
+<?php echo page_footer(); ?>
