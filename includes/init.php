eb45c894035bac0e2d192ea31fa70636c70c86ad
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 16:35:57 +0100

    update

---
+++ includes/init.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['config'] = require $_SERVER['DOCUMENT_ROOT'].'/includes/config.php';
+
+require $_SERVER['DOCUMENT_ROOT'].'/includes/oauth.php';
+require $_SERVER['DOCUMENT_ROOT'].'/includes/security.php';
+require $_SERVER['DOCUMENT_ROOT'].'/includes/sql.php';
+require $_SERVER['DOCUMENT_ROOT'].'/includes/template.php';
---
+++ includes/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return (object) [
+    'database' => (object) [
+        'host' => 'localhost',
+        'user' => 'overlangbroek_db',
+        'password' => 'PASSWORD',
+        'database' => 'overlangbroek_db',
+    ],
+];
