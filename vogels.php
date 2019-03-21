6dda663d6601c43e531bb3eba939bbd15ef36ab2
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 18:13:49 +0100

    list vogels functionality

--- vogels.php
+++ vogels.php
@@ -1,6 +1,26 @@
 <?php require $_SERVER['DOCUMENT_ROOT'].'/includes/init.php'; ?>
 <?php echo page_header('Vogels || Overlangbroek', 'vogels'); ?>
 
-<?php echo json_encode(sql_select('vogels', '*', true, true)); ?>
+<?php
+
+while ($vogel = sql_select('vogels', 'id,name,img_url', true, false)->fetch_assoc()) {
+    echo <<<HTML
+    <div class="row">
+        <div class="col s12 m3">
+            <a href="/vogels/{$vogel['id']}"
+                ><div class="card">
+                    <div class="card-image">
+                        <img src="{$vogel['img_url']}" />
+                    </div>
+                    <div class="card-content smallbox">
+                        <span class="card-title black-text">{$vogel['name']}</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+HTML;
+}
+?>
 
 <?php echo page_footer(); ?>
