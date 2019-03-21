a157f43e55408bcc6079d78bbe04196af53dd9d9
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 17:58:40 +0100

    update

--- includes/init.php
+++ includes/init.php
@@ -1,7 +1,7 @@
 <?php
 
-// $GLOBALS['config'] = require $_SERVER['DOCUMENT_ROOT'].'/includes/config.php';
+$GLOBALS['config'] = require $_SERVER['DOCUMENT_ROOT'].'/includes/settings.php';
 
 // require $_SERVER['DOCUMENT_ROOT'].'/includes/security.php';
-// require $_SERVER['DOCUMENT_ROOT'].'/includes/sql.php';
+require $_SERVER['DOCUMENT_ROOT'].'/includes/sql.php';
 require $_SERVER['DOCUMENT_ROOT'].'/includes/template.php';
