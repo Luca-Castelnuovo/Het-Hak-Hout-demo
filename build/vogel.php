979d80a63d8a07d8827734ba51a070bf7107ae78
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-21 18:38:00 +0100

    update

--- build/vogel.php
+++ build/vogel.php
@@ -1,6 +1,12 @@
 <?php require $_SERVER['DOCUMENT_ROOT'].'/includes/init.php'; ?>
 <?php echo page_header('VOGELNAAM || Overlangbroek', 'vogels'); ?>
 
+<?php
+
+$vogel = sql_select('vogels', 'name,img_url,description', 'id = '.$id, true);
+
+?>
+
 <div class="row">
     <!-- Phone -->
     <div class="hide-on-med-and-up center">
@@ -9,14 +15,14 @@
                 <div class="card-panel">
                     <div class="row">
                         <div class="col s12">
-                            <img src="static/images/Buizerd2.jpg" class="circle" width="200">
+                            <img src="<?php echo $vogel['img_url']; ?>" class="circle" width="200">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col s12">
-                            <h2 class="mt-0">Buizerd</h2>
+                            <h2 class="mt-0"><?php echo $vogel['name']; ?></h2>
                             <p>
-                                De buizerd is een middelgrote tot grote roofvogel uit de familie van de havikachtigen. De wetenschappelijke naam van de soort werd als Falco buteo in 1758 gepubliceerd door Carl Linnaeus. De buizerd komt voor in het grootste gedeelte van Europa en delen van Azië. Hij is overwegend een standvogel die in hetzelfde gebied overwintert als waar hij broedt, behalve in de koudste gebieden en op enkele ondersoorten na. De vogel jaagt gebruikelijk in open land, maar nestelt in bosranden. Normaal gesproken bestaat de prooi van een buizerd voornamelijk uit kleine zoogdieren, amfibieën en kleine vogels, maar hij is bij gelegenheid ook aaseter.
+                                <?php echo $vogel['description']; ?>
                             </p>
                         </div>
                     </div>
@@ -32,14 +38,14 @@
                 <div class="card-panel">
                     <div class="row mb-0">
                         <div class="col s5">
-                            <img src="/static/images/Buizerd2.jpg" onerror="this.src='https://cdn.lucacastelnuovo.nl/general/images/profile_picture.png'" class="responsive-img">
+                            <img src="<?php echo $vogel['img_url']; ?>" class="responsive-img">
                         </div>
                         <div class="col s7">
                             <div class="row">
                                 <div class="col s12">
-                                    <h2>Buizerd</h2>
+                                    <h2><?php echo $vogel['name']; ?></h2>
                                     <p>
-                                        De buizerd is een middelgrote tot grote roofvogel uit de familie van de havikachtigen. De wetenschappelijke naam van de soort werd als Falco buteo in 1758 gepubliceerd door Carl Linnaeus. De buizerd komt voor in het grootste gedeelte van Europa en delen van Azië. Hij is overwegend een standvogel die in hetzelfde gebied overwintert als waar hij broedt, behalve in de koudste gebieden en op enkele ondersoorten na. De vogel jaagt gebruikelijk in open land, maar nestelt in bosranden. Normaal gesproken bestaat de prooi van een buizerd voornamelijk uit kleine zoogdieren, amfibieën en kleine vogels, maar hij is bij gelegenheid ook aaseter.
+                                        <?php echo $vogel['description']; ?>
                                     </p>
                                 </div>
                             </div>
